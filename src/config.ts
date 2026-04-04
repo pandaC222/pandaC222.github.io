c0d54012c47fd3e5d48c536da5e31a4457e37ea8
pandaC222 <3036432922@qq.com>
2026-04-04 18:15:26 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -201,7 +201,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// 留空以使用默认 favicon
 		{
-		  src: '/favicon/logo.png',    // 图标文件路径
+		  src: '/favicon/QQ20260404-181343.png',    // 图标文件路径
 		  theme: 'light',              // 可选，指定主题 'light' | 'dark'
 		  sizes: '32x32',              // 可选，图标大小
 		}
