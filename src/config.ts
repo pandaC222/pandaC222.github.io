806135e41d359652d84677415b8b6758e8cfc026
pandaC222 <3036432922@qq.com>
2026-04-04 21:33:23 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -148,7 +148,7 @@ export const siteConfig: SiteConfig = {
 
 		waves: {
 			enable: true, // 是否启用水波纹效果（注意：此功能性能开销较大）
-			performanceMode: true, // 性能模式：减少动画复杂度(性能提升40%)
+			performanceMode: false, // 性能模式：减少动画复杂度(性能提升40%)
 			mobileDisable: false, // 移动端禁用
 		},
 
