d145b90dc1d68b5406c88d16179e11dc564bcf48
pandaC222 <3036432922@qq.com>
2026-04-04 14:22:39 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -369,8 +369,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "测试1",
-	bio: "测试2",
+	name: "pandac",
+	bio: "希望有朝一日成为算法大蛇",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
